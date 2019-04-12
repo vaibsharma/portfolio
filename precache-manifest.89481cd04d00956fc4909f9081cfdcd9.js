83c53761c0a3b99a89b801ce4e122aa12fc77268
vaibsharma <vaib.sharma44@gmail.com>
2019-04-12 23:34:58 +0530

    Updates

---
+++ precache-manifest.89481cd04d00956fc4909f9081cfdcd9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1180163e67bd5a5664b",
+    "url": "/portfolio/static/css/main.6437d5c6.chunk.css"
+  },
+  {
+    "revision": "b1180163e67bd5a5664b",
+    "url": "/portfolio/static/js/main.96d16d3b.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "bcf003abaf9b68c3ca8b",
+    "url": "/portfolio/static/js/2.7c618a4b.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "50e15935a6ef301881500ddca3e18f2c",
+    "url": "/portfolio/index.html"
+  }
+];
